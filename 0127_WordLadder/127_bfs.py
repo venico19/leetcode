6c2cfef5b9c0c55091598bf28cbad986f374e204
Jing <pingjing@gmail.com>
2019-06-01 20:04:35 -0500

    127

---
+++ 0127_WordLadder/127_bfs.py
@@ -0,0 +1,32 @@
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        if not beginWord or not endWord or not wordList:
+            return 0
+        wordSet = set(wordList)
+        if endWord not in wordSet:
+            return 0
+        
+        visited = set([beginWord])
+        
+        queue = collections.deque()
+        queue.append((beginWord, 1))
+        
+        while queue:
+            w, depth = queue.popleft()
+            for transform in self.wordTransform(w):
+                if transform in visited:
+                    continue
+                if transform in wordSet:
+                    visited.add(transform)
+                    if transform == endWord:
+                        return depth + 1
+                    queue.append((transform, depth + 1))
+        return 0
+    
+    def wordTransform(self, word):
+        res = []
+        for i in range(len(word)):
+            for char in 'abcdefghijklmnopqrstuvwxyz':
+                res.append(word[:i] + char + word[i+1:])
+        return res
+    
