eb5893ee243327a96497281f1efdd0f5db88993b
Jing <pingjing@gmail.com>
2019-06-28 18:33:28 -0500

    227

--- 0227_BasicCalculatorII/227_solution.py
+++ 0227_BasicCalculatorII/227_solution.py
@@ -1,25 +1,30 @@
 class Solution:
     def calculate(self, s: str) -> int:
-        stack = []
-        temp = 0
-        sign = '+'
-        for i, char in enumerate(s):
-            if char.isdigit():
-                temp = temp * 10 + int(char)
-            if char in '+-*/' or i == len(s) - 1:
-                if sign == '+':
-                    stack.append(temp)
-                elif sign == '-':
-                    stack.append(-temp)
-                elif sign == '*':
-                    stack.append(stack.pop() * temp)
+        def update(op, num):
+            if op == '+':
+                stack.append(num)
+            elif op == '-':
+                stack.append(-num)
+            elif op == '*':
+                stack.append(stack.pop() * num)
+            elif op == '/':
+                value = stack.pop()
+                if value // num < 0 and value % num != 0:
+                    stack.append(value // num + 1)
                 else:
-                    value = stack.pop()
-                    if value // temp < 0 and value % temp != 0:
-                        stack.append(value//temp + 1)
-                    else:
-                        stack.append(value//temp)
-                sign = char
-                temp = 0
-      
+                    stack.append(value // num)
+                    
+        stack = []
+        num = 0
+        op = '+'
+        for char in s:
+            if char.isdigit():
+                num = num * 10 + int(char)
+            elif char in '+-*/':
+                update(op, num)
+                num = 0
+                op = char
+        update(op, num)
+                
         return sum(stack)
+            
